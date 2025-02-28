fdd0ed56f1199d3c3baa8a75c0ccbc414fba09da
lucasmnascimentostarttech <lucasmnascimento.starttech@gmail.com>
2025-02-28 17:19:41 -0500

    Update msg_control.h

--- inc/msg_control.h
+++ inc/msg_control.h
@@ -1,7 +1,7 @@
 #include "includes.h"
 
 
-int getMessage(Clients *client);
+int getMessage(Clients *client, ClientsArray *clients);
 int sendMessage(Clients *client, ClientsArray *clients);
 int sendWelcomeMessage(Clients *client);
 int recvMsg(Connection *conct);
